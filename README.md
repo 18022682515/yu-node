511b966e11e497522fc002d69607008a45e871ac
邓小宇 <625071541@qq.com>
2019-05-04 16:37:26 +0800

    更新

--- README.md
+++ README.md
@@ -8,15 +8,21 @@ npm install yu-node --save
   
 **引入**
 ```
-const { emptyDir, rmDir } = require('yu-node');
+const { emptyDir,rmDir,getAllFiles } = require('yu-node');
 ```
   
-**`emptyDir(dirPath)`：清空目录，完成返回true，否则返回false**
+### emptyDir(dirPath)`：清空目录，完成返回true，否则返回false
 ```
 let bool = emptyDir('./目录a');  //true 或 false
 ```
   
-**`rmDir(dirPath)`：清空并删除目录，完成返回true，否则返回false**
+### `rmDir(dirPath)`：清空并删除目录，完成返回true，否则返回false
 ```
 let bool = rmDir('./目录a');  //true 或 false
+```
+
+### `getAllFiles(dirName)`：获取目录下的所有文件名
+```
+let arr = getAllFiles('./目录名');  
+//[ 'D:\\demo\\node-babel\\src\\foo\\a.css','D:\\demo\\node-babel\\src\\foo\\b.js',]
 ```
--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const { emptyDir,rmDir } = require('./lib/handleDir.js');
+const { getAllFiles } = require('./lib/getAllFiles.js');
 
-const obj = {};
-
-function emptyDir(dir){
-    
-    const files = fs.readdirSync(dir, {withFileTypes:true});
-    //说明：withFileTypes:true, arr的元素是dirent对象，dirent可以判断：本资源是目录还是文件
-    if(files.length<1){
-        return;
-    };
-    files.forEach((dirent,index)=>{
-        if(dirent.isFile()){   //判断本资源是不是文件
-            fs.unlinkSync(dir+'/'+dirent.name);
-        }else{
-            emptyDir(dir+'/'+dirent.name);
-            fs.rmdirSync(dir+'/'+dirent.name);
-        }
-    });
-}
-
-obj.emptyDir = dirPath => {
-    const bool = fs.existsSync(dirPath); //判断目录是否存在
-    if(!bool) return false;
-
-    emptyDir(dirPath); //清空目录的子孙目录和所有文件
-    return true;
-};
-
-obj.rmDir = dirPath => {
-    const bool = fs.existsSync(dirPath); //判断目录是否存在
-    if(!bool) return false;
-
-    emptyDir(dirPath); //清空目录的子孙目录和所有文件
-    fs.rmdirSync(dirPath);  //删除本目录
-    return true;
-}
-
-module.exports = Object.freeze(obj);
+module.exports = Object.freeze({ emptyDir,rmDir,getAllFiles });
